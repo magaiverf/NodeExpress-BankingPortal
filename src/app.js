fdf88b2750355ad2a954f4e38b6be5418a9ca807
Magaiver Oliveira <magaiverf@gmail.com>
2022-02-08 18:07:40 -0300

    Add the code regarding the module3

--- src/app.js
+++ src/app.js
@@ -9,9 +9,32 @@ app.set('view engine', 'ejs'); // seta qual é o view engine do projeto
 
 app.use(express.static(path.join(__dirname, 'public'))); // informa qual é a pasta de arquivo estatico
 
+// lê o arquivo e converte para um JS Object
+const accountData = fs.readFileSync(path.join(__dirname, 'json/accounts.json'), { encoding: 'utf8'});
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync(path.join(__dirname, 'json/users.json'), { encoding: 'utf8'});
+const users = JSON.parse(userData);
+
 // cria uma rota get para o endereço '/'
 app.get('/', (req, res) => {
-    res.render('index', {title: 'Index'}); // como estamos usando uma engine ele renderiza a pagina Index e passa um objeto para ela.
+    res.render('index', {title: 'Account Summary', accounts}); // como estamos usando uma engine ele renderiza a pagina Index e passa um objeto para ela.
+});
+
+app.get('/savings', (req, res) => {
+    res.render('account', {account: accounts.savings});
+});
+
+app.get('/checking', (req, res) => {
+    res.render('account', {account: accounts.checking});
+});
+
+app.get('/credit', (req, res) => {
+    res.render('account', {account: accounts.credit});
+});
+
+app.get('/profile', (req, res) => {
+    res.render('profile', {user: users[0]}); 
 });
 
 app.listen(3000, () => {
