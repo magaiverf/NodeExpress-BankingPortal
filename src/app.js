ab54503e23b61a02ec0cb0d94f035c3a712359b4
Magaiver Oliveira <magaiverf@gmail.com>
2022-02-09 19:47:26 -0300

    Add changes regarding the real module 3

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ app.set('views', path.join(__dirname, 'views')); // informa onde está a pasta d
 app.set('view engine', 'ejs'); // seta qual é o view engine do projeto
 
 app.use(express.static(path.join(__dirname, 'public'))); // informa qual é a pasta de arquivo estatico
+app.use(express.urlencoded({extended: true})); // ?
 
 // lê o arquivo e converte para um JS Object
 const accountData = fs.readFileSync(path.join(__dirname, 'json/accounts.json'), { encoding: 'utf8'});
@@ -36,6 +37,45 @@ app.get('/credit', (req, res) => {
 app.get('/profile', (req, res) => {
     res.render('profile', {user: users[0]}); 
 });
+
+app.get('/transfer', (req, res) => {
+    res.render('transfer'); 
+});
+
+app.post('/transfer', (req, res) => {
+    const data = req.body;
+    const accountFromType = data.from;
+    const accountToType = data.to;
+
+    const amount = parseInt(data.amount); // como recebemos uma string do formulário, precisamos converter para int
+
+    accounts[accountFromType].balance = accounts[accountFromType].balance - amount;
+    accounts[accountToType].balance = accounts[accountToType].balance + amount;
+
+    let accountsJSON = JSON.stringify(accounts);
+
+    // escreve em um arquivo o valor de accountsJSON
+    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+
+    res.render('transfer', { message: 'Transfer Completed'});
+});
+
+app.get('/payment', (req, res) => {
+    res.render('payment', { account: accounts.credit});
+});
+
+app.post('/payment', (req, res) => {
+    const amount = parseInt(req.body.amount);
+
+    accounts.credit.balance = accounts.credit.balance - amount;
+    accounts.credit.available = accounts.credit.available + amount;
+
+    const accountsJSON = JSON.stringify(accounts);
+    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+
+    res.render('payment', { message: "Payment Successful", account: accounts.credit });
+});
+
 
 app.listen(3000, () => {
     console.log('PS Prject Running on port 3000!');
